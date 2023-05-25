2a06e4c03295592889c7865272feed965d2b0801
Hanasaki_Fubuki <50268837+hanasaki-fubuki@users.noreply.github.com>
2023-05-25 13:04:27 +0800

    upload icon

--- vue/src/components/Cards/CardCourseManage.vue
+++ vue/src/components/Cards/CardCourseManage.vue
@@ -12,210 +12,210 @@
     <a-collapse accordion>
       <a-collapse-panel key="1" header="1.1 数据库系统三级模式结构">
         <a-upload :file-list="fileList1" action="1" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="2" header="1.2 ER图及向关系的转换">
         <a-upload :file-list="fileList2" action="2" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="3" header="2.1 关系的性质和完整性约束">
         <a-upload :file-list="fileList3" action="3" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="4" header="2.2 关系代数的基本运算">
         <a-upload :file-list="fileList4" action="4" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="5" header="2.3 关系代数练习">
         <a-upload :file-list="fileList5" action="5" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="6" header="3.1 SQL概述-实验环境及DDL">
         <a-upload :file-list="fileList6" action="6" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="7" header="3.2 SQL的数据查询">
         <a-upload :file-list="fileList7" action="7" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="8" header="3.3 SQL的数据更新">
         <a-upload :file-list="fileList8" action="8" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="9" header="3.4 视图">
         <a-upload :file-list="fileList9" action="9" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="10" header="3.5 授予与回收权限">
         <a-upload :file-list="fileList10" action="10" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="11" header="4.1 游标">
         <a-upload :file-list="fileList11" action="11" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="12" header="4.2 存储过程和函数">
         <a-upload :file-list="fileList12" action="12" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="13" header="5.1 数据库的完整性">
         <a-upload :file-list="fileList13" action="13" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="14" header="5.2 在被参照关系中删除元组时的问题">
         <a-upload :file-list="fileList14" action="14" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="15" header="5.3 外码是否可以接受空值的问题">
         <a-upload :file-list="fileList15" action="15" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="16" header="5.4 修改被参照关系中主码的问题">
         <a-upload :file-list="fileList16" action="16" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="17" header="5.5 在参照关系中插入元组时的问题">
         <a-upload :file-list="fileList17" action="17" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="18" header="5.6 触发器">
         <a-upload :file-list="fileList18" action="18" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="19" header="6.1 关系数据理论基本概念">
         <a-upload :file-list="fileList19" action="19" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="20" header="6.2 范式分析">
         <a-upload :file-list="fileList20" action="20" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="21" header="6.3 函数依赖的公理系统">
         <a-upload :file-list="fileList21" action="21" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="22" header="6.4 求候选码">
         <a-upload :file-list="fileList22" action="22" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="23" header="6.5 模式分解">
         <a-upload :file-list="fileList23" action="23" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="24" header="7.1 事务的基本概念及ACID特性">
         <a-upload :file-list="fileList24" action="24" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="25" header="7.2 三种故障及其恢复">
         <a-upload :file-list="fileList25" action="25" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="26" header="7.3 转储和日志文件的基本概念">
         <a-upload :file-list="fileList26" action="26" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="27" header="7.4 具有检查点的恢复技术和数据库镜像">
         <a-upload :file-list="fileList27" action="27" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="28" header="8.1 并发调度的可串行性和两段锁协议">
         <a-upload :file-list="fileList28" action="28" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="29" header="8.2 活锁和死锁">
         <a-upload :file-list="fileList29" action="29" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
       </a-collapse-panel>
       <a-collapse-panel key="30" header="8.3 封锁粒度">
         <a-upload :file-list="fileList30" action="30" :customRequest="handleUpload" @preview="handleDownload" :remove="handleRemove">
-          <a-button>
+          <a-button><a-icon type="upload" />
             上传本课程相关文件
           </a-button>
         </a-upload>
@@ -227,11 +227,9 @@
 </template>
 
 <script>
-import {UploadOutlined} from '@ant-design/icons-vue';
 
 export default {
     components: {
-      UploadOutlined,
     },
 		props: {
 			data: {
